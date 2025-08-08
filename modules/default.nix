051aa71f7b991eaed464d79a11682143d73b9f4d
Lucas DRAESCHER <lucas.draescher@gmail.com>
2025-08-08 15:35:45 +0200

    default: replace btop with htop

--- modules/default.nix
+++ modules/default.nix
@@ -17,7 +17,6 @@
     bat
     bitwarden-cli
     bitwarden-desktop
-    btop
     delta
     dig
     direnv
@@ -29,6 +28,7 @@
     git-lfs
     gnumake
     go-task
+    htop
     jq
     postgresql
     pspg
