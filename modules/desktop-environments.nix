264c6bab71b63049e29b71a14f9e0a333ecb725d
Lucas DRAESCHER <lucas.draescher@gmail.com>
2025-08-08 15:45:03 +0200

    desktop-environments: remove niri

--- modules/desktop-environments.nix
+++ modules/desktop-environments.nix
@@ -85,10 +85,4 @@
       eval $(gnome-keyring-daemon --start --daemonize)
     '';
   };
-
-  programs.niri.enable = true;
-  environment.systemPackages = with pkgs; [
-    fuzzel
-    xwayland-satellite
-  ];
 }
