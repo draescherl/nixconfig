fa6a02b6ba8c3f48d3faba20651d5299e1314934
Lucas DRAESCHER <git@draescher.fr>
2025-12-21 23:01:47 +0100

    security: add pinentry program for gpg agent

--- modules/security.nix
+++ modules/security.nix
@@ -1,9 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
   security.polkit.enable = true;
   services.pcscd.enable = true;
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
+    pinentryPackage = pkgs.pinentry-qt;
   };
 }
