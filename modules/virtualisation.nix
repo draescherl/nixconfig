6aaa93f3bb6cb2d3dee51062f60b04ef89c9faac
Lucas DRAESCHER <git@draescher.fr>
2026-06-12 12:48:14 +0200

    exherbo: disable container startup on boot

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -18,7 +18,7 @@
       # override systemd-nspawn service for exherbo container to use host networking & DNS
       ExecStart = lib.mkForce "systemd-nspawn --bind-ro=/etc/resolv.conf --quiet --keep-unit --boot --link-journal=try-guest -U --settings=override --machine=%i";
     };
-    wantedBy = [ "multi-user.target" ]; # Start on boot
+    #wantedBy = [ "multi-user.target" ]; # Start on boot
   };
 
   users.users.${username}.packages = with pkgs; [
