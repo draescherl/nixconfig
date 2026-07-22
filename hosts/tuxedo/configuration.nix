a57e1a3b9befce658957df063c8c464e677630f6
Lucas DRAESCHER <git@draescher.fr>
2026-07-22 04:31:44 +0200

    tuxedo: add fix for read-only FS when resuming from lock

--- hosts/tuxedo/configuration.nix
+++ hosts/tuxedo/configuration.nix
@@ -1,9 +1,14 @@
-{ config, pkgs, ... }:
-
+{ ... }:
 {
   imports = [
-      ./hardware-configuration.nix
-    ];
+    ./hardware-configuration.nix
+  ];
+
+  # Fix for read-only filesystem on resume after lock/suspend.
+  services.udev.extraRules = ''
+    SUBSYSTEM=="pci", ACTION=="add", ATTR{vendor}=="0x144d", ATTR{device}=="0xa80a", RUN+="/bin/sh -c 'echo 0 | tee /sys/bus/pci/devices/$kernel/d3cold_allowed'"
+    SUBSYSTEM=="pci", ACTION=="add", ATTR{vendor}=="0x144d", ATTR{device}=="0xa808", RUN+="/bin/sh -c 'echo 0 | tee /sys/bus/pci/devices/$kernel/d3cold_allowed'"
+  '';
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -12,5 +17,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "26.05"; # Did you read the comment?
-
 }
