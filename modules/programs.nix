91db5009358f35b0241bfba30d5c9c42cd811688
Lucas DRAESCHER <git@draescher.fr>
2026-09-16 11:26:52 +0200

    programs: stop nh from running garbage collection automatically
    It would clean tools used in devshells a bit too aggressively

--- modules/programs.nix
+++ modules/programs.nix
@@ -4,12 +4,7 @@
   ...
 }:
 {
-  programs.nh = {
-    enable = true;
-    clean.enable = true;
-    clean.extraArgs = "--keep-since 4d --keep 3";
-  };
-
+  programs.nh.enable = true;
   programs.fish.enable = true;
 
   environment.systemPackages =
